1249d2520bf4481dfa29937bce424ee8961a4bd5
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-12-16 12:33:02 +0100

    secure book

--- add_book.php
+++ add_book.php
@@ -3,8 +3,8 @@
 $bdd = new PDO("mysql:host=localhost;dbname=revision_php_securite", "root", "root");
 $sql = "INSERT INTO book (title, writer, abstract) VALUES (:title, :writer, :abstract)";
 $requete = $bdd->prepare($sql);
-$requete->bindValue(":title", $_POST['title'], PDO::PARAM_STR);
-$requete->bindValue(":writer", $_POST['writer'], PDO::PARAM_STR);
-$requete->bindValue(":abstract", $_POST['abstract'], PDO::PARAM_STR);
+$requete->bindValue(":title", strip_tags($_POST['title']), PDO::PARAM_STR);
+$requete->bindValue(":writer", strip_tags($_POST['writer']), PDO::PARAM_STR);
+$requete->bindValue(":abstract", strip_tags($_POST['abstract']), PDO::PARAM_STR);
 $requete->execute();
 header('Location: index.php');
--- update_book.php
+++ update_book.php
@@ -3,10 +3,10 @@
 $bdd = new PDO("mysql:host=localhost;dbname=revision_php_securite", 'root', 'root');
 $sql = "UPDATE book SET title = :title, writer = :writer, abstract = :abstract WHERE id = :id";
 $requete = $bdd->prepare($sql);
-$requete->bindValue(":title", $_POST['title'], PDO::PARAM_STR);
-$requete->bindValue(":writer", $_POST['writer'], PDO::PARAM_STR);
-$requete->bindValue(":abstract", $_POST['abstract'], PDO::PARAM_STR);
-$requete->bindValue(":id", $_POST['id'], PDO::PARAM_INT);
+$requete->bindValue(":title", strip_tags($_POST['title']), PDO::PARAM_STR);
+$requete->bindValue(":writer", strip_tags($_POST['writer']), PDO::PARAM_STR);
+$requete->bindValue(":abstract", strip_tags($_POST['abstract']), PDO::PARAM_STR);
+$requete->bindValue(":id", strip_tags($_POST['id']), PDO::PARAM_INT);
 $requete->execute();
 
 var_dump($_POST);
